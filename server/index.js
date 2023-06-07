b3003e515e5ea79598c53be05bb4b0f51cff3830
Luis Carrasco <luis@8ventures.io>
2023-06-07 22:54:34 +0200

    feat: client initialized

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const host = 'localhost';
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(
   session({
     name: 'sid',
